4fd4a2a6089281c9d5568b296e7cc67db830bcf8
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2020-09-26 16:36:34 +0200

    Prepare for demo

--- src/Dotnet2020.Api/Features/Security/CustomerDto.cs
+++ src/Dotnet2020.Api/Features/Security/CustomerDto.cs
@@ -1,11 +1,7 @@
-﻿using AspNetCore.Hashids.Json;
-using System.Text.Json.Serialization;
-
-namespace Dotnet2020.Api.Features.Security
+﻿namespace Dotnet2020.Api.Features.Security
 {
     public class CustomerDto
     {
-        [JsonConverter(typeof(HashidsJsonConverter))]
         public int Id { get; set; }
         public string Name { get; set; }
     }
