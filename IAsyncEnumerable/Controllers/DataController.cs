00a8bbf4b4543cd05bdd0060337badbbf127f2d5
Carlos Recuero Díaz <crecuero@plainconcepts.com>
2020-10-01 07:21:53 +0200

    Create projects

---
+++ IAsyncEnumerable/Controllers/DataController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IAsyncEnumerable.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        private readonly ILogger<DataController> _logger;
+
+        public DataController(ILogger<DataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(GetData());
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        static async Task<IEnumerable<int>> GetData()
+        {
+            var datas = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(500);
+                datas.Add(i);
+            }
+
+            return datas;
+        }
+
+        static async IAsyncEnumerable<int> GetDataAsyncEnum()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(500);
+                yield return i;
+            }
+        }
+    }
+}
---
+++ IAsyncEnumerableConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerableConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("GetData: Pulse cualquier tecla para comenzar");
+            Console.ReadKey();
+            Console.WriteLine("GetData: Comenzado");
+
+            foreach (var data in await GetData())
+            {
+                Console.WriteLine(data);
+            }
+
+            Console.WriteLine("GetData: Finalizado");
+
+            Console.WriteLine("GetDataAsyncEnum: Pulse cualquier tecla para comenzar");
+            Console.ReadKey();
+            Console.WriteLine("GetDataAsyncEnum: Comenzado");
+
+            await foreach(var data in GetDataAsyncEnum())
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("GetDataAsyncEnum: Finalizado");
+
+            Console.ReadLine();
+        }
+
+        static async Task<IEnumerable<int>> GetData()
+        {
+            var datas = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(500);
+                datas.Add(i);
+            }
+
+            return datas;
+        }
+
+        static async IAsyncEnumerable<int> GetDataAsyncEnum()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(500);
+                yield return i;
+            }
+        }
+    }
+}
